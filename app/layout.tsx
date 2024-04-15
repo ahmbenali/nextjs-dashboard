fb876a1f94c9b6bf7b5049a47b19c08c65bfe8a4
Ahmed Benali <abenali.dev@gmail.com>
2024-04-15 21:14:05 +0200

    import global.css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
-import '@/app/ui/global.css';
 import { PropsWithChildren } from 'react';
+import '~/app/ui/global.css';
 import { inter } from './ui/fonts';
 
 function RootLayout({ children }: PropsWithChildren) {
